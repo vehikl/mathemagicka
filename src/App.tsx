4c89a8d58ed01e64225aa7879b2416723c0bee21
joel-smith <joel.craig.smith@gmail.com>
2022-08-08 17:08:57 -0400

    update with growth session 1 work

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import '.'
+import MagickInput from "./Components/MagickInput";
+import Calculator from "./Calculator";
+
+function evaluate(input: string) {
+    return Calculator.evaluate(input);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <span>mathemagicka</span>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <span>mathemagicka</span>
+
+            <MagickInput />
+        </div>
+    );
 }
 
 export default App;
