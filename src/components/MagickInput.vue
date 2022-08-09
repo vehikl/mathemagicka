4e0b22760ac034dd0c56b4f4f850c13901230151
joel-smith <joel.craig.smith@gmail.com>
2022-08-08 22:33:56 -0400

    removed old project work

---
+++ src/components/MagickInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+
+    <input v-model="question" placeholder="edit me" />
+    <button @click="execute">ask</button>
+
+    <p>Question is: {{ question }}</p>
+    <p>Answer is: {{
+        answer
+      }}</p>
+  </div>
+
+
+</template>
+
+<script>
+import Calculator from "@/Calculator";
+
+export default {
+  name: "MagickInput",
+  data() {
+    return {
+      question: 'tmptaco',
+      answer: 'tmpanswer'
+    }
+  },
+  methods: {
+    execute() {
+      this.answer = Calculator.evaluate(this.question);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
